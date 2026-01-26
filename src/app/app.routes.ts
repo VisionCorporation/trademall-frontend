6308e8f4ab1d197468de1b1ffa2632a1d6adfb43
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-01-26 12:21:25 +0000

    feat: define routing for customer signup page with redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'customer/signup',
+        pathMatch: 'full',
+    },
+    {
+        path: 'customer/signup',
+        loadComponent: () => import('./pages/customer-signup/customer-signup').then(m => m.CustomerSignup)
+    }
+];
