a9a9b779c9c16b0f5d0c4e87e8c9795d28bbbf00
kwadwoambitious <antwiebenezer824@gmail.com>
2026-01-29 15:21:12 +0000

    chore: add otp success response interface

--- src/app/interfaces/signup.interface.ts
+++ src/app/interfaces/signup.interface.ts
@@ -1,8 +1,15 @@
 export interface SignupData {
-  email: string;
+  userId: string;
   firstName: string;
   lastName: string;
   phoneNumber: string;
   password: string;
   confirmPassword: string;
+}
+
+export interface OtpSuccessResponse {
+  userId: string;
+  success: boolean;
+  email: string;
+  message: string;
 }
