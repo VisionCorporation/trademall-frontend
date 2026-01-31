f52289d5e9aae5abe9a62b353c0ce55f7343a589
kwadwoambitious <antwiebenezer824@gmail.com>
2026-01-31 13:00:58 +0000

    feat: add animations and toast component to improve user experience

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -10,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
-  ]
+    provideAnimations()
+  ],
 };
