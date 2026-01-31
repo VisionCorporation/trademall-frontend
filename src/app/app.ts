5d7539c6cd5e1ed70732962ff0e384b4d94ba4ab
kwadwoambitious <antwiebenezer824@gmail.com>
2026-01-31 16:23:41 +0000

    add: implement login check on component initialization

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from './shared/toast/toast';
+import { LoginService } from './services/login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,13 @@ import { Toast } from './shared/toast/toast';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('TradeMall');
+  private readonly loginService = inject(LoginService);
+
+  ngOnInit() {
+    if (this.loginService.isLoggedIn()) {
+      console.log('Session restored');
+    }
+  }
 }
