4438a066f1cda60cadb023aa2464da5317f242c7
kwadwoambitious <antwiebenezer824@gmail.com>
2026-02-05 20:14:22 +0000

    refactor: lazy load home component on root path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'customer/signup',
-    pathMatch: 'full',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
   },
   {
     path: 'customer/signup',
