8027a0a2bbdadb11f3248dd623850775313f57b3
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-02 16:59:24 +0000

    feat: add loading indicator for products fetching in AllProducts page

--- src/app/pages/all-products/all-products.ts
+++ src/app/pages/all-products/all-products.ts
@@ -1,31 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { PRODUCTS } from '../../data/constants/products.constant';
 import { CurrencyPipe } from '@angular/common';
 import { Products } from '../../services/products/products';
 import { Footer } from '../../shared/footer/footer';
-import { smoothCollapse } from '../../animations/smooth-collapse.animations';
+import { smoothCollapse, staggerProducts } from '../../animations/smooth-collapse.animations';
+import { SkeletonLoader } from '../../shared/skeleton-loader/skeleton-loader';
 
 @Component({
   selector: 'app-all-products',
-  imports: [Header, Footer, CurrencyPipe],
+  imports: [Header, Footer, CurrencyPipe, SkeletonLoader],
   templateUrl: './all-products.html',
   styleUrl: './all-products.css',
-  animations: [smoothCollapse],
+  animations: [smoothCollapse, staggerProducts],
 })
 export class AllProducts implements OnInit {
-  public readonly products = PRODUCTS;
+  public products = PRODUCTS;
   private readonly productService = inject(Products);
   public isCategoryOpen = false;
-
+  public isProductsLoading = signal(false);
   ngOnInit() {
-    this.productService.getAllCategories().subscribe((categories) => {
-      console.log('Categories:', categories);
-    });
-
-    this.productService.getAllProducts().subscribe((products) => {
-      console.log('Products:', products);
-    });
+    this.isProductsLoading.set(true);
+    setTimeout(() => {
+      this.isProductsLoading.set(false);
+    }, 2000);
   }
 
   public toggleCategory() {
