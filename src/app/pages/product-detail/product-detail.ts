5ddacf6a06281e6e229b7485f5b028054439c9ab
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-12 13:28:02 +0000

    feat: add toast notification for failed product fetch

--- src/app/pages/product-detail/product-detail.ts
+++ src/app/pages/product-detail/product-detail.ts
@@ -1,19 +1,21 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Products } from '../../services/products/products';
 import { Product } from '../../interfaces/products.interface';
 import { CurrencyPipe } from '@angular/common';
-
+import { ToastService } from '../../services/toast/toast.service';
+import { SkeletonLoader } from '../../shared/skeleton-loader/skeleton-loader';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [RouterLink, CurrencyPipe],
+  imports: [CurrencyPipe, SkeletonLoader],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
 export class ProductDetail implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly productService = inject(Products);
+  private readonly toastService = inject(ToastService);
 
   public product: Product | null = null;
   public isLoading = signal(true);
@@ -29,9 +31,10 @@ export class ProductDetail implements OnInit {
           this.isLoading.set(false);
         },
         error: (err) => {
+          this.toastService.error('An error occurred while fetching product details. Try again');
           console.error('Failed to fetch product', err);
           this.isLoading.set(false);
-        }
+        },
       });
     }
   }
