7102f399cab7633bf275b42d8d8ebccd18aaead4
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-16 02:24:31 +0000

    feat: add styling for custom checkboxes and watch tutorial button in category products page

---
+++ src/app/pages/category-products/category-products.ts
@@ -0,0 +1,82 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Products } from '../../services/products/products';
+import { SkeletonLoader } from '../../shared/skeleton-loader/skeleton-loader';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
+import { CurrencyPipe } from '@angular/common';
+import { Product } from '../../interfaces/products.interface';
+import { staggerProducts } from '../../animations/smooth-collapse.animations';
+
+@Component({
+  selector: 'app-category-products',
+  imports: [SkeletonLoader, Header, Footer, RouterLink, CurrencyPipe],
+  templateUrl: './category-products.html',
+  styleUrl: './category-products.css',
+  animations: [staggerProducts],
+})
+export class CategoryProducts implements OnInit {
+  public isProductsLoading = signal(true);
+  public isSubCategoriesLoading = signal(true);
+  public openCategorySlug: string | null = null;
+  public selectedCategorySlug: string | null = null;
+  public selectedCategoryProducts: Product[] = [];
+  private readonly productService = inject(Products);
+  private readonly route = inject(ActivatedRoute);
+  public subCategories: any[] = [];
+  public categoryName = '';
+
+  ngOnInit() {
+    const slug = this.route.snapshot.paramMap.get('slug');
+
+    if (slug) {
+      this.productService.getCategoryWithDirectChildren(slug).subscribe({
+        next: (res) => {
+          this.categoryName = res.data.category.name;
+          this.subCategories = res.data.children;
+          this.isProductsLoading.set(false);
+          this.isSubCategoriesLoading.set(false);
+
+          if (this.subCategories.length > 0) {
+            this.selectedCategorySlug = this.subCategories[0].slug;
+            this.fetchProductsByCategory(this.selectedCategorySlug!);
+          } else {
+            this.isProductsLoading.set(false);
+          }
+        },
+        error: (err) => {
+          console.error('Failed to fetch product', err);
+          this.isProductsLoading.set(false);
+          this.isSubCategoriesLoading.set(false);
+        },
+      });
+    }
+  }
+
+  public onCategoryChange(slug: string, event: Event): void {
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    if (isChecked) {
+      this.selectedCategorySlug = slug;
+      this.fetchProductsByCategory(slug);
+    } else {
+      this.selectedCategorySlug = null;
+      this.selectedCategoryProducts = [];
+    }
+  }
+
+  private fetchProductsByCategory(categorySlug: string): void {
+    this.isProductsLoading.set(true);
+
+    this.productService.getProductsByCategory(categorySlug).subscribe({
+      next: (products) => {
+        this.selectedCategoryProducts = products.data;
+        this.isProductsLoading.set(false);
+      },
+      error: (err) => {
+        console.error('Failed to fetch products', err);
+        this.isProductsLoading.set(false);
+      },
+    });
+  }
+}
