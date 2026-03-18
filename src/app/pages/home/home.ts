44c07e77cedb0b22463b9f8df449acf45f01ae5a
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-18 06:15:15 +0000

    feat: add newsletter and footer components to home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { RouterLink } from '@angular/router';
+import { Newsletter } from '../../shared/newsletter/newsletter';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, RouterLink],
+  imports: [Header, RouterLink, Newsletter, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
