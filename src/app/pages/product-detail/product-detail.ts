97a5bfd529e538fc5a4480d2c0e8964715946852
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-20 11:26:20 +0000

    feat: add Breadcrumb, Header, and Footer components to Product Detail page

--- src/app/pages/product-detail/product-detail.ts
+++ src/app/pages/product-detail/product-detail.ts
@@ -5,10 +5,13 @@ import { Product } from '../../interfaces/products.interface';
 import { CurrencyPipe } from '@angular/common';
 import { ToastService } from '../../services/toast/toast.service';
 import { SkeletonLoader } from '../../shared/skeleton-loader/skeleton-loader';
+import { Breadcrumb } from '../../shared/breadcrumb/breadcrumb';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [CurrencyPipe, SkeletonLoader, RouterLink],
+  imports: [CurrencyPipe, SkeletonLoader, RouterLink, Breadcrumb, Header, Footer],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
@@ -16,18 +19,23 @@ export class ProductDetail implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly productService = inject(Products);
   private readonly toastService = inject(ToastService);
+  public categoryName = '';
+  public categorySlug = '';
+  public selectedSubCategorySlug = ''
 
   public product: Product | null = null;
   public isLoading = signal(true);
 
   ngOnInit(): void {
     const slug = this.route.snapshot.paramMap.get('slug');
+    this.categoryName = history.state?.category;
+    this.categorySlug = history.state?.slug;
+    this.selectedSubCategorySlug = history.state?.subcategorySlug;
 
     if (slug) {
       this.productService.getProductBySlug(slug).subscribe({
         next: (response) => {
           this.product = response.data;
-          console.log('Fetched product:', this.product);
           this.isLoading.set(false);
         },
         error: (err) => {
