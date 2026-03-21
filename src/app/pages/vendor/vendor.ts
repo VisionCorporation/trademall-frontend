53610be5b47fb61ff1e2e5fd80c319d31fb470cd
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-21 14:02:36 +0000

    feat: add wishlist functionality and notifications in vendor page

--- src/app/pages/vendor/vendor.ts
+++ src/app/pages/vendor/vendor.ts
@@ -3,13 +3,16 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Products } from '../../services/products/products';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
-import { VendorProductsResponse } from '../../interfaces/vendor.interface';
-import { CurrencyPipe, DatePipe } from '@angular/common';
+import { VendorInfo, VendorProductsResponse } from '../../interfaces/vendor.interface';
+import { CurrencyPipe, DatePipe, NgOptimizedImage } from '@angular/common';
 import { staggerProducts } from '../../animations/smooth-collapse.animations';
+import { ToastService } from '../../services/toast/toast.service';
+import { Newsletter } from '../../shared/newsletter/newsletter';
+import { SkeletonLoader } from "../../shared/skeleton-loader/skeleton-loader";
 
 @Component({
   selector: 'app-vendor',
-  imports: [Header, Footer, DatePipe, RouterLink, CurrencyPipe],
+  imports: [Header, Footer, DatePipe, RouterLink, CurrencyPipe, Newsletter, NgOptimizedImage, SkeletonLoader],
   templateUrl: './vendor.html',
   styleUrl: './vendor.css',
   animations: [staggerProducts],
@@ -18,7 +21,10 @@ export class Vendor {
   private productService = inject(Products);
   private readonly route = inject(ActivatedRoute);
   public vendorData: VendorProductsResponse | null = null;
+  public vendorInfo: VendorInfo | null = null;
   public isLoading = signal(true);
+  public wishlistedIds = new Set<string>();
+  private readonly toastService = inject(ToastService);
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -27,15 +33,25 @@ export class Vendor {
       this.productService.getVendorProductsById(id).subscribe({
         next: (response) => {
           this.vendorData = response;
-          // console.log('Fetched vendor data:', this.vendorData);
+          this.vendorInfo = response.vendor
           this.isLoading.set(false);
         },
         error: (err) => {
-          // this.toastService.error('An error occurred while fetching vendor details. Try again');
           console.error('Failed to fetch vendor data', err);
+          this.toastService.error("An occurred while fetching vendor's details. Try again")
           this.isLoading.set(false);
         },
       });
+    }
+  }
+
+  public toggleWishlist(productId: string, productName: string = ''): void {
+    if (this.wishlistedIds.has(productId)) {
+      this.wishlistedIds.delete(productId);
+      this.toastService.success(`${productName} removed from wishlist`);
+    } else {
+      this.wishlistedIds.add(productId);
+      this.toastService.success(`${productName} added to wishlist`);
     }
   }
 }
