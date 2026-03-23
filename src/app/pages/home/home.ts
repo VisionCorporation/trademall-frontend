ac2e2fdb45cd212d9918a601ed0b1ba2894b46e2
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-23 15:52:56 +0000

    feat: add FeaturedCategories component to home page layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,13 +3,14 @@ import { Header } from '../../shared/header/header';
 import { RouterLink } from '@angular/router';
 import { Newsletter } from '../../shared/newsletter/newsletter';
 import { Footer } from '../../shared/footer/footer';
+import { FeaturedCategories } from '../../shared/featured-categories/featured-categories';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, RouterLink, Newsletter, Footer],
+  imports: [Header, RouterLink, Newsletter, Footer, FeaturedCategories],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+ 
 }
