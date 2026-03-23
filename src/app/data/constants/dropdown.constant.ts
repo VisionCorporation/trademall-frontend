bb4829bee51a44394bbd8e1886a6f714b54b1e96
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-23 13:33:50 +0000

    fix: update 'Order History' dropdown URL for correct navigation

--- src/app/data/constants/dropdown.constant.ts
+++ src/app/data/constants/dropdown.constant.ts
@@ -2,7 +2,7 @@ export const CustomerDropdown = [
   { label: 'Profile', url: '' },
   { label: 'Cart', url: '' },
   { label: 'Wishlist', url: '' },
-  { label: 'Order History', url: '' },
+  { label: 'Order History', url: '/order-history' },
   { label: 'Notifications', url: '' },
   { label: 'Location', url: '' },
 ];
