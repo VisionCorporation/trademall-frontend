feb61ba3b2f3b60673b00d5b80d2f6cbff399e92
kwadwoambitious <antwiebenezer824@gmail.com>
2026-03-23 13:33:15 +0000

    feat: add order history page with empty state and animations for better user experience

--- src/app/pages/customer-order-history/customer-order-history.ts
+++ src/app/pages/customer-order-history/customer-order-history.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { RouterLink } from "@angular/router";
+import { fadeInOutAnimation } from '../../animations/toast.animations';
+import { Newsletter } from '../../shared/newsletter/newsletter';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-customer-order-history',
-  imports: [],
+  imports: [Header, RouterLink, Newsletter, Footer],
   templateUrl: './customer-order-history.html',
   styleUrl: './customer-order-history.css',
+  animations:[fadeInOutAnimation]
 })
 export class CustomerOrderHistory {
-
+  public isOrderPlaced = false;
 }
