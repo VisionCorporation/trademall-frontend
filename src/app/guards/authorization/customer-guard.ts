25c8367ee59ba9ca433ddde52bd751996070616f
kwadwoambitious <antwiebenezer824@gmail.com>
2026-04-06 01:44:32 +0000

    refactor: wait for session to load before authorization check in customer guard

--- src/app/guards/authorization/customer-guard.ts
+++ src/app/guards/authorization/customer-guard.ts
@@ -7,6 +7,7 @@ import {
 import { LoginService } from '../../services/login/login.service';
 import { inject } from '@angular/core';
 import { ToastService } from '../../services/toast/toast.service';
+import { filter, map, take } from 'rxjs';
 
 export const customerGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
@@ -16,12 +17,18 @@ export const customerGuard: CanActivateFn = (
   const toastService = inject(ToastService);
   const router = inject(Router);
 
-  const user = authService.getCurrentUser();
+  return authService.sessionLoaded$.pipe(
+    filter((loaded) => loaded === true),
+    take(1),
+    map(() => {
+      const user = authService.getCurrentUser();
 
-  if (user && user.userType === 'customer') {
-    return true;
-  }
+      if (user && user.userType === 'customer') {
+        return true;
+      }
 
-  toastService.error('You are not authorized to access this page');
-  return router.createUrlTree(['/']);
+      toastService.error('You are not authorized to access this page');
+      return router.createUrlTree(['/']);
+    }),
+  );
 };
