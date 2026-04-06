f590e6c1da8334a807f97e0bf6679caca596f849
kwadwoambitious <antwiebenezer824@gmail.com>
2026-04-06 01:44:46 +0000

    feat: improve auth guard to wait for session load before redirecting

--- src/app/guards/authentication/auth-guard.ts
+++ src/app/guards/authentication/auth-guard.ts
@@ -6,17 +6,23 @@ import {
 } from '@angular/router';
 import { LoginService } from '../../services/login/login.service';
 import { inject } from '@angular/core';
+import { filter, map, take } from 'rxjs';
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot, 
+  state: RouterStateSnapshot,
 ) => {
   const authService = inject(LoginService);
   const router = inject(Router);
 
-  if (authService.isLoggedIn()) {
-    return true;
-  }
-
-  return router.createUrlTree(['/login']);
+  return authService.sessionLoaded$.pipe(
+    filter((loaded) => loaded === true),
+    take(1),
+    map(() => {
+      if (authService.isLoggedIn()) {
+        return true;
+      }
+      return router.createUrlTree(['/login']);
+    }),
+  );
 };
