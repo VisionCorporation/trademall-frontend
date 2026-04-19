295d68a032dc5adecc2dd13965c3123611902faa
kwadwoambitious <antwiebenezer824@gmail.com>
2026-04-19 00:28:13 +0000

    feat: add product buttons constant for product details page

---
+++ src/app/data/constants/product-details.constant.ts
@@ -0,0 +1,14 @@
+export const buttons = [
+  {
+    label: 'About Product',
+    value: 'about-product',
+  },
+  {
+    label: 'Product Details',
+    value: 'product-details',
+  },
+  {
+    label: 'Reviews',
+    value: 'reviews',
+  },
+];
