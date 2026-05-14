bb5a3dd59c18a81f83b08807dd69ddadebcc18f8
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-14 22:13:26 +0000

    feat: implement product card component with template, styles, and unit tests

---
+++ src/app/shared/product-card/product-card/product-card.ts
@@ -0,0 +1,19 @@
+import { Component, inject, Input } from '@angular/core';
+import { CartState } from '../../../services/cart/cart-state';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+import { Wishlist } from '../../../services/wishlist/wishlist/wishlist';
+import { ProductCardInterface } from '../../../interfaces/products.interface';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [RouterLink, CurrencyPipe],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  @Input() product!: ProductCardInterface | FeaturedProduct
+
+  public cartStateService = inject(CartState);
+  public wishlistService = inject(Wishlist)
+}
