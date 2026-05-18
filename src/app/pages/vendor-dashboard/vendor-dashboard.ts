085c1b20a68a9d845b3b745fa74da52b4ab47a13
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-18 20:00:31 +0000

    feat: implement logout functionality in vendor dashboard

--- src/app/pages/vendor-dashboard/vendor-dashboard.ts
+++ src/app/pages/vendor-dashboard/vendor-dashboard.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NAV_ITEMS } from '../../data/constants/vendor-dashbaord.constant';
+import { LoginService } from '../../services/login/login.service';
+import { ToastService } from '../../services/toast/toast.service';
 
 @Component({
   selector: 'app-vendor-dashboard',
@@ -10,4 +12,13 @@ import { NAV_ITEMS } from '../../data/constants/vendor-dashbaord.constant';
 })
 export class VendorDashboard {
   public navItems = NAV_ITEMS
+  private readonly router = inject(Router);
+  private readonly loginService = inject(LoginService)
+  private readonly toastService = inject(ToastService)
+
+  public logout() {
+    this.loginService.logout();
+    this.router.navigate(['/login']);
+    this.toastService.success("You've logged out successfully!");
+  }
 }
