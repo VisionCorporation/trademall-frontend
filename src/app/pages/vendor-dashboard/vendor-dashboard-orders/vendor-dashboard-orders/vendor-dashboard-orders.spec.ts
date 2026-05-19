0efeb875d02f65a85cac066b2b495f19d813b953
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-19 15:57:05 +0000

    feat: add vendor dashboard orders component with HTML, CSS, and unit tests

---
+++ src/app/pages/vendor-dashboard/vendor-dashboard-orders/vendor-dashboard-orders/vendor-dashboard-orders.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VendorDashboardOrders } from './vendor-dashboard-orders';
+
+describe('VendorDashboardOrders', () => {
+  let component: VendorDashboardOrders;
+  let fixture: ComponentFixture<VendorDashboardOrders>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VendorDashboardOrders]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VendorDashboardOrders);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/vendor-dashboard/vendor-dashboard-orders/vendor-dashboard-orders/vendor-dashboard-orders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-dashboard-orders',
+  imports: [],
+  templateUrl: './vendor-dashboard-orders.html',
+  styleUrl: './vendor-dashboard-orders.css',
+})
+export class VendorDashboardOrders {
+
+}
