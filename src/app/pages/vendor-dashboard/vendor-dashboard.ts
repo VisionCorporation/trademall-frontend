3feef70acdd7bd481c20d62b956746830dab4594
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-19 11:52:59 +0000

    feat: implement OnInit lifecycle hook to manage user state in vendor dashboard

--- src/app/pages/vendor-dashboard/vendor-dashboard.ts
+++ src/app/pages/vendor-dashboard/vendor-dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NAV_ITEMS } from '../../data/constants/vendor-dashbaord.constant';
 import { LoginService } from '../../services/login/login.service';
@@ -10,11 +10,22 @@ import { ToastService } from '../../services/toast/toast.service';
   templateUrl: './vendor-dashboard.html',
   styleUrl: './vendor-dashboard.css',
 })
-export class VendorDashboard {
+export class VendorDashboard implements OnInit {
   public navItems = NAV_ITEMS
   private readonly router = inject(Router);
   private readonly loginService = inject(LoginService)
   private readonly toastService = inject(ToastService)
+  public user: any
+
+  ngOnInit() {
+    this.loginService.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  get userInitials(): string {
+    return this.user ? `${this.user.firstName[0]}${this.user.lastName[0]}` : '';
+  }
 
   public logout() {
     this.loginService.logout();
