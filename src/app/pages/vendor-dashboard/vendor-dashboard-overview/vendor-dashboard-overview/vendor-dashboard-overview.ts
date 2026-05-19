9f49d40f4b10b5f84db286c821c69265fd29bd0c
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-19 15:45:09 +0000

    feat: enhance vendor dashboard overview with product statistics and loading state

--- src/app/pages/vendor-dashboard/vendor-dashboard-overview/vendor-dashboard-overview/vendor-dashboard-overview.ts
+++ src/app/pages/vendor-dashboard/vendor-dashboard-overview/vendor-dashboard-overview/vendor-dashboard-overview.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { VendorDashboard } from '../../../../services/vendor-dashboard/vendor-dashboard';
+import { Product } from '../../../../interfaces/vendor-dashboard.interface';
+import { PRODUCT_STATUS_CONFIG } from '../../../../data/constants/vendor-dashbaord.constant';
+import { SkeletonLoader } from '../../../../shared/skeleton-loader/skeleton-loader';
 
 @Component({
   selector: 'app-vendor-dashboard-overview',
-  imports: [],
+  imports: [CurrencyPipe, DatePipe, SkeletonLoader],
   templateUrl: './vendor-dashboard-overview.html',
   styleUrl: './vendor-dashboard-overview.css',
 })
 export class VendorDashboardOverview {
+  public periods = ['Weekly', 'Monthly', 'Yearly'];
+  public selectedPeriod = 'Monthly';
+  public isFetchingProducts = signal(false);
+  private vendorDashboardService = inject(VendorDashboard);
+  public fetchedProducts: Product[] = [];
+  public statusConfig = PRODUCT_STATUS_CONFIG;
 
+  revenue: Record<string, number> = {
+    Weekly: 4320,
+    Monthly: 17750,
+    Yearly: 213000,
+  };
+
+  ngOnInit(): void {
+    this.fetchVendorProductsListings();
+  }
+
+  private fetchVendorProductsListings(): void {
+    this.isFetchingProducts.set(true);
+
+    this.vendorDashboardService.getVendorProductListings().subscribe({
+      next: (response) => {
+        this.fetchedProducts = response.data.slice(3, 6);
+        this.isFetchingProducts.set(false);
+      },
+      error: (error) => {
+        console.error('Error fetching vendor product listings:', error);
+        this.isFetchingProducts.set(false);
+      }
+    });
+  }
 }
