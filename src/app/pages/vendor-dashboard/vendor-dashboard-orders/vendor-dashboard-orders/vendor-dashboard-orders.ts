2d35db77960a2400435c32ae43801d31a947da52
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-22 12:19:13 +0000

    feat: implement period selection dropdown in vendor dashboard orders

--- src/app/pages/vendor-dashboard/vendor-dashboard-orders/vendor-dashboard-orders/vendor-dashboard-orders.ts
+++ src/app/pages/vendor-dashboard/vendor-dashboard-orders/vendor-dashboard-orders/vendor-dashboard-orders.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
-import { ORDERS, ORDERS_FILTERS, ORDERS_STATUS_CONFIG } from '../../../../data/constants/vendor-dashbaord.constant';
+import { ORDER_PERIODS, ORDERS, ORDERS_FILTERS, ORDERS_STATUS_CONFIG } from '../../../../data/constants/vendor-dashbaord.constant';
+import { ClickOutside } from '../../../../directives/click-outside/click-outside';
+import { fadeInOutAnimation } from '../../../../animations/toast.animations';
 
 @Component({
   selector: 'app-vendor-dashboard-orders',
-  imports: [],
+  imports: [ClickOutside],
   templateUrl: './vendor-dashboard-orders.html',
   styleUrl: './vendor-dashboard-orders.css',
+  animations: [fadeInOutAnimation],
 })
 export class VendorDashboardOrders {
   public ordersFilters = ORDERS_FILTERS
   public orders = ORDERS
+  public orderPeriods = ORDER_PERIODS
   public statusConfig = ORDERS_STATUS_CONFIG
   public selectedFilter = 'all-orders'
   public hoveredFilter: string | null = null;
   public isSelectOpen = false;
+  public selectedPeriod = 'All Orders';
+
+  public selectPeriod(period: { label: string; value: string }) {
+    this.selectedPeriod = period.label;
+    this.isSelectOpen = false;
+  }
 
   public selectFilter(filterValue: string): void {
     this.selectedFilter = filterValue;
