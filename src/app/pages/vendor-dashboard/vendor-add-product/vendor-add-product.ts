b1795a17560f6590199fc33570bb03e12457bc7b
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-05-25 13:47:38 +0000

    feat: enhance vendor add product page with dynamic category and subcategory loading

--- src/app/pages/vendor-dashboard/vendor-add-product/vendor-add-product.ts
+++ src/app/pages/vendor-dashboard/vendor-add-product/vendor-add-product.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { CATEGORIES } from '../../../data/constants/vendor-dashbaord.constant';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { fadeInOutAnimation } from '../../../animations/toast.animations';
 import { ClickOutside } from '../../../directives/click-outside/click-outside';
+import { Products } from '../../../services/products/products';
+import { Category, RootCategory } from '../../../interfaces/categories.interface';
 
 @Component({
   selector: 'app-vendor-add-product',
@@ -10,13 +11,66 @@ import { ClickOutside } from '../../../directives/click-outside/click-outside';
   styleUrl: './vendor-add-product.css',
   animations: [fadeInOutAnimation]
 })
-export class VendorAddProduct {
-  public categories = CATEGORIES
+export class VendorAddProduct implements OnInit {
+  public categories: RootCategory[] = []
+  public subCategories: Category[] = []
   public isCategoryOpen = false
-  public selectedCategory = ''
+  public isSubCategoryOpen = false
+  public selectedCategory: RootCategory | null = null
+  public selectedSubCategory: Category | null = null
+  private readonly categoryService = inject(Products)
+  public isCategoryLoading = signal(false)
+  public isSubCategoryLoading = signal(false)
+  public hasCategoryFailed = signal(false);
+  public hasSubCategoryFailed = signal(false);
 
-  public selectCategory(category: { label: string; value: string }) {
-    this.selectedCategory = category.label;
+  ngOnInit() {
+    this.fetchCategories()
+  }
+
+  public fetchCategories() {
+    this.isCategoryLoading.set(true)
+    this.hasCategoryFailed.set(false);
+    this.categoryService.getRootCategories(1).subscribe({
+      next: (response) => {
+        this.categories = response.data;
+        this.isCategoryLoading.set(false);
+      },
+      error: (err) => {
+        console.error('Failed to fetch categories', err);
+        this.isCategoryLoading.set(false);
+        this.hasCategoryFailed.set(true);
+      },
+    });
+  }
+
+  public fetchSubCategories() {
+    if (this.selectedCategory) {
+      this.isSubCategoryLoading.set(true)
+      this.hasSubCategoryFailed.set(false);
+      this.categoryService.getSubCategories(this.selectedCategory.slug).subscribe({
+        next: (response) => {
+          this.subCategories = response.data;
+          this.isSubCategoryLoading.set(false);
+        },
+        error: (err) => {
+          console.error('Failed to fetch subcategories', err);
+          this.isSubCategoryLoading.set(false);
+          this.hasSubCategoryFailed.set(true);
+        },
+      });
+    }
+  }
+
+  public selectCategory(category: RootCategory) {
+    this.selectedCategory = category;
+    this.selectedSubCategory = null;
+    this.fetchSubCategories();
     this.isCategoryOpen = false;
+  }
+
+  public selectSubCategory(subCategory: Category) {
+    this.selectedSubCategory = subCategory;
+    this.isSubCategoryOpen = false;
   }
 }
