9c95ae4cc01bb47d4e8ec2f1570b792b82a9f2c3
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-08-27 17:26:25 +0000

    feat: enhance footer component with social links and dynamic sections

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { FOOTER_LINKS } from '../../data/constants/footer.constant';
+import { FooterSection } from '../../interfaces/footer.interface';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   public readonly currentYear = new Date().getFullYear();
+  public footerSections: FooterSection[] = FOOTER_LINKS
+
+  public openSection: string | null = null;
+
+  public toggleSection(title: string): void {
+    this.openSection = this.openSection === title ? null : title;
+  }
 }
