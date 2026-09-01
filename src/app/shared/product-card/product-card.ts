adcc833a3513c388a3b1b609516c939c1d832b5f
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-09-01 17:41:03 +0000

    feat: refactor addToCart method and implement priceSnapshot getter in ProductCard component

--- src/app/shared/product-card/product-card.ts
+++ src/app/shared/product-card/product-card.ts
@@ -5,6 +5,7 @@ import { CurrencyPipe } from '@angular/common';
 import { Wishlist } from '../../services/wishlist/wishlist';
 import { ProductCardInterface, ProductDetails } from '../../interfaces/products.interface';
 import { Product } from '../../interfaces/all-products.interface';
+import { PriceSnapshot } from '../../interfaces/cart.interface';
 
 @Component({
   selector: 'app-product-card',
@@ -17,4 +18,18 @@ export class ProductCard {
   @Input() layout: 'grid' | 'carousel' = 'grid';
   public cartStateService = inject(CartState);
   public wishlistService = inject(Wishlist)
+
+  public get priceSnapshot(): PriceSnapshot {
+    const salePrice = (this.product as any).salePrice ?? null;
+    return {
+      productName: this.product.name,
+      price: this.product.price,
+      salePrice,
+      effectivePrice: salePrice ?? this.product.price,
+    };
+  }
+
+  public addToCart(): void {
+    this.cartStateService.addToCart(this.product._id, 1, this.priceSnapshot);
+  }
 }
