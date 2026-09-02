fb2d26827765dd1d9d7ab8ce022c30d9def06cae
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-09-02 01:18:49 +0000

    feat: add additional server routes for vendor and user management

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,18 +3,42 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'products/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: 'categories/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: 'products/vendor/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'vendor/**',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'vendor-application-status',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'order-history',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'manage-addresses',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
