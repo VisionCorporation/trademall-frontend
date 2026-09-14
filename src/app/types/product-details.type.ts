91b8e54e32412a777f73fa43483dc9cae95d87a2
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-09-14 18:22:48 +0000

    feat: add UpdateReviewPayload type for review updates

--- src/app/types/product-details.type.ts
+++ src/app/types/product-details.type.ts
@@ -1,1 +1,4 @@
+import { ReviewFormData } from "../interfaces/reviews.interface";
+
 export type VoteType = 'helpful' | 'not_helpful';
+export type UpdateReviewPayload = Partial<Pick<ReviewFormData, 'rating' | 'title' | 'comment'>>;
