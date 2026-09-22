d93caed1a1b3ac8bf09973f437be2a2e2fac12c9
Ebenezer Antwi <antwiebenezer824@gmail.com>
2026-09-22 14:45:44 +0000

    feat: add ConfirmDialog component to app module and template

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from './shared/toast/toast';
 import { LoginService } from './services/login/login.service';
+import { ConfirmDialog } from './shared/confirm-dialog/confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast],
+  imports: [RouterOutlet, Toast, ConfirmDialog],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
